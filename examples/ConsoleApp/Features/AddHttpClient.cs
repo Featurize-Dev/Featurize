eaff97e5c394be38f23df7912376380cfb7f10da
Patrick Evers <pmdevers@gmail.com>
2022-12-02 16:54:43 +0100

    Update Console example to use nuget package

--- examples/ConsoleApp/Features/AddHttpClient.cs
+++ examples/ConsoleApp/Features/AddHttpClient.cs
@@ -1,10 +1,5 @@
 ﻿using Featurize.ConsoleApp;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp.Features;
 public sealed class AddHttpClient : IConfigureFeature
