0618d613b5e084994594e3498c32987f82b06702
Patrick Evers <pmdevers@gmail.com>
2022-12-03 21:50:26 +0100

    Add xml comments (#3)
    * Add xml comments
    
    * build metadata

--- src/Featurize.AspNetCore.WebAssembly/IWebAssemblyHostFeature.cs
+++ src/Featurize.AspNetCore.WebAssembly/IWebAssemblyHostFeature.cs
@@ -1,19 +1,39 @@
-﻿using Featurize;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace Featurize.AspNetCore.WebAssembly;
 
+/// <summary>
+/// Describes a web assembly feature.
+/// </summary>
+/// <seealso cref="IConfigureFeature" />
+/// <seealso cref="IUseFeature" />
 public interface IWebAssemblyHostFeature : IConfigureFeature, IUseFeature
 {
 
 }
 
+/// <summary>
+/// Describes a feature that can manipulate the <see cref="WebAssemblyHostBuilder"/>.
+/// </summary>
+/// <seealso cref="IFeature" />
 public interface IConfigureFeature : IFeature
 {
+    /// <summary>
+    /// Configures the specified builder.
+    /// </summary>
+    /// <param name="builder">The builder.</param>
     void Configure(WebAssemblyHostBuilder builder);
 }
 
+/// <summary>
+/// Describes a feature that can influence the request pipeline.
+/// </summary>
+/// <seealso cref="Featurize.IFeature" />
 public interface IUseFeature : IFeature
 {
+    /// <summary>
+    /// Uses the specified application.
+    /// </summary>
+    /// <param name="app">The application.</param>
     void Use(WebAssemblyHost app);
 }
--- src/Featurize.AspNetCore/IWebApplicationFeature.cs
+++ src/Featurize.AspNetCore/IWebApplicationFeature.cs
@@ -1,19 +1,39 @@
-﻿using Featurize;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 
 namespace Featurize.AspNetCore;
 
+/// <summary>
+/// Describes a web application feature.
+/// </summary>
+/// <seealso cref="Featurize.AspNetCore.IConfigureFeature" />
+/// <seealso cref="Featurize.AspNetCore.IUseFeature" />
 public interface IWebApplicationFeature : IConfigureFeature, IUseFeature
 {
 
 }
 
+/// <summary>
+/// Describes a feature to register services.
+/// </summary>
+/// <seealso cref="Featurize.IFeature" />
 public interface IConfigureFeature : IFeature
 {
+    /// <summary>
+    /// Allow the feature to register services
+    /// </summary>
+    /// <param name="builder">The builder.</param>
     void Configure(WebApplicationBuilder builder);
 }
 
+/// <summary>
+/// Describes a feature to add to the request pipeline.
+/// </summary>
+/// <seealso cref="Featurize.IFeature" />
 public interface IUseFeature : IFeature
 {
+    /// <summary>
+    /// Allow the feature to add to the request pipeline.
+    /// </summary>
+    /// <param name="app">The application.</param>
     public void Use(WebApplication app);
 }
