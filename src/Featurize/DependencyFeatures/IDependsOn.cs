675ccb24a88c1b91e1b0a29305ceef892c3d70c1
Patrick Evers <pmdevers@gmail.com>
2023-03-24 15:17:24 +0100

    Sort Features on dependencies

---
+++ src/Featurize/DependencyFeatures/IDependsOn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Featurize.SortFeatures;
+public interface IDependsOn<TFeature>
+    where TFeature : IFeature
+{
+}
