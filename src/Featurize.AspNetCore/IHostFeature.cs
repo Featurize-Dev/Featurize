f8c16899804261aff55282f6cd83dc09ccd57cfd
Patrick Evers <pmdevers@gmail.com>
2023-11-03 00:14:02 +0100

    Introduce HostBuilder Feature

---
+++ src/Featurize.AspNetCore/IHostFeature.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Featurize.AspNetCore;
+
+/// <summary>
+/// Describes a HostBuilder feature.
+/// </summary>
+/// <seealso cref="Featurize.IFeature" />
+public interface IHostFeature : IFeature
+{
+    /// <summary>
+    /// Allow the feature to configure the Host specific properties.
+    /// </summary>
+    /// <param name="hostBuilder">The Host builder.</param>
+    void Configure(IHostBuilder hostBuilder);
+}
